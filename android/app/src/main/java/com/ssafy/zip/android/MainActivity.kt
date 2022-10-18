13a3ed1196c344913603655e56c867f06c2de6d4
이승연 <seongyeon38@gmail.com>
2022-10-18 18:42:11 +0900

    FE_221018_bottomtab: bottomtab, navigation 구현 완료

--- android/app/src/main/java/com/ssafy/zip/android/MainActivity.kt
+++ android/app/src/main/java/com/ssafy/zip/android/MainActivity.kt
@@ -2,10 +2,21 @@ package com.ssafy.zip.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ssafy.zip.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
