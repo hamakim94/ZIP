3080164010e6df05d1f637786187fbe1723188b8
이보나 <bona17@naver.com>
2022-10-25 14:50:36 +0900

    FE_221025_notification: 화면 구성 완료

--- android/app/src/main/java/com/ssafy/zip/android/NotificationFragment.kt
+++ android/app/src/main/java/com/ssafy/zip/android/NotificationFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.zip.android.data.Notification
