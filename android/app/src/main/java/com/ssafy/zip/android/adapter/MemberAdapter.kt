a12c48da238e090d1a94b89722e8bf278ad1deee
이도엽 <lx460@naver.com>
2022-10-28 10:52:29 +0900

    FE_221028_login : 데이터 클래스 생성중

--- android/app/src/main/java/com/ssafy/zip/android/adapter/MemberAdapter.kt
+++ android/app/src/main/java/com/ssafy/zip/android/adapter/MemberAdapter.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.zip.android.R
-import com.ssafy.zip.android.data.Member
+import com.ssafy.zip.android.data.User
 import de.hdodenhof.circleimageview.CircleImageView
 //fragmentManager: FragmentManager
-class MemberAdapter (private val memberList :ArrayList<Member>) : RecyclerView.Adapter<MemberAdapter.MemberViewHolder>(){
+class MemberAdapter (private val userList :ArrayList<User>) : RecyclerView.Adapter<MemberAdapter.MemberViewHolder>(){
 //    private var mFragmentManager : FragmentManager
 //    init {
 //        mFragmentManager = fragmentManager
@@ -30,8 +28,8 @@ class MemberAdapter (private val memberList :ArrayList<Member>) : RecyclerView.A
     }
 
     override fun onBindViewHolder(holder: MemberViewHolder, position: Int) {
-        val memberImage = memberList[position]
-        holder.memberImage.setImageResource(memberImage.image)
+        val memberImage = userList[position]
+        holder.memberImage.setImageResource(memberImage.profileImg.toInt())
         holder.memberImage.setOnClickListener {
 //            val dialog = CustomDialog()
             val args = Bundle()
@@ -44,7 +42,7 @@ class MemberAdapter (private val memberList :ArrayList<Member>) : RecyclerView.A
 
 
     override fun getItemCount(): Int {
-        return memberList.size
+        return userList.size
     }
 
 }
