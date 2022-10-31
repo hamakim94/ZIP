0d90c7069e6d6c3209d5aa46c544058b8f729bda
이도엽 <lx460@naver.com>
2022-11-01 07:21:56 +0900

    FE_221031_home : 홈 연동 완료

--- android/app/src/main/java/com/ssafy/zip/android/ApiInterface.kt
+++ android/app/src/main/java/com/ssafy/zip/android/ApiInterface.kt
@@ -1,22 +1,19 @@
 package com.ssafy.zip.android
 
-import com.ssafy.zip.android.data.Album
-import com.ssafy.zip.android.data.Photo
-import com.ssafy.zip.android.data.Family
-import com.ssafy.zip.android.data.Missions
-import com.ssafy.zip.android.data.UserFamily
-import com.ssafy.zip.android.data.User
+import com.ssafy.zip.android.data.*
 import com.ssafy.zip.android.data.request.RequestFamilyroom
 import com.ssafy.zip.android.data.request.RequestLoginData
 import com.ssafy.zip.android.data.request.RequestPhoto
-import okhttp3.MultipartBody
+import com.ssafy.zip.android.data.request.RequestSignup
 import com.ssafy.zip.android.data.response.ResponseBoardAll
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.*
-import com.ssafy.zip.android.data.request.RequestSignup
 
 interface ApiInterface {
+    // 유저 관련(초기화면)
     @POST("users/login")//Post Interface
     suspend fun userLogin(
         @Body body: RequestLoginData
@@ -36,6 +33,16 @@ interface ApiInterface {
     suspend fun emailCheck(
         @Query(value = "email") email : String,
     ):Response<String>
+
+    @PUT("rooms/enter")
+    suspend fun enterRoom(
+        @Body code: Int,
+    ):Response<UserFamily>
+
+    @POST("rooms/create")
+    suspend fun createFamily(
+        @Body body: RequestFamilyroom
+    ) : Response<UserFamily>
 
     // 앨범 관련
     @POST("album")
@@ -63,22 +70,24 @@ interface ApiInterface {
         @Part("pictureRequestDTO") photoList : RequestPhoto
     ) : Response<List<Photo>>
 
-    @PUT("rooms/enter")
-    suspend fun enterRoom(
-        @Body code: Int,
-    ):Response<UserFamily>
-
-    @POST("rooms/create")
-    suspend fun createFamily(
-        @Body body: RequestFamilyroom
-    ) : Response<UserFamily>
-
+    // 홈 화면 관련
     @GET("rooms")
     suspend fun getFamily():Response<Family>
+
+    @GET("post/missions")
+    suspend fun getMission():Response<Missions>
+
+    @Multipart
+    @PUT("users/profiles")
+    suspend fun modifyUser(
+        @Part profileImg: MultipartBody.Part?,
+        @Part("familyName") familyName : RequestBody,
+        @Part("nickname") nickname : RequestBody
+    ): Response<User>
+
 
     @GET("post")
     fun getBoard() : Call<List<ResponseBoardAll>>
 
-    @GET("post/missions")
-    suspend fun getMission():Response<Missions>
+
 }
