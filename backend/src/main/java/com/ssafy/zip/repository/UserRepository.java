34d4754c67eef9a11908b55689b2ab04552655ed
류현수 <neillie23@gmail.com>
2022-11-01 22:06:21 +0900

    BE_221101_Debug : Modifying Method Return Value Fault (완료)

--- backend/src/main/java/com/ssafy/zip/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/zip/repository/UserRepository.java
@@ -13,5 +13,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 
-    List<Family> findByFamily_Id(Long familyId);
+    List<User> findByFamily_Id(Long familyId);
 }
