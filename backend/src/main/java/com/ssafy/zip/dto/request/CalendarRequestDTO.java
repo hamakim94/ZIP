600d2edb3e5041bfa1f29c9a50919a0c9ee47d62
이재순 <dlwotns1112@gmail.com>
2022-11-02 17:19:19 +0900

    BE_221102_calendar : 롤백 완료

--- backend/src/main/java/com/ssafy/zip/dto/request/CalendarRequestDTO.java
+++ backend/src/main/java/com/ssafy/zip/dto/request/CalendarRequestDTO.java
@@ -1,6 +1,5 @@
 package com.ssafy.zip.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,9 +13,7 @@ import java.util.List;
 @Builder
 @ToString
 public class CalendarRequestDTO {
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS][.SS][.S]")
     LocalDateTime startDate;
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss[.SSS][.SS][.S]")
     LocalDateTime endDate;
     String content;
     List<Long> userIds;
