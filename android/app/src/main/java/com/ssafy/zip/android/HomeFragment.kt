2791ec010472e6c405134b9e93d4782619042806
이도엽 <lx460@naver.com>
2022-11-04 10:34:02 +0900

    FE_221104_fix : 로그아웃 수정

--- android/app/src/main/java/com/ssafy/zip/android/HomeFragment.kt
+++ android/app/src/main/java/com/ssafy/zip/android/HomeFragment.kt
@@ -16,7 +16,11 @@ import com.ssafy.zip.android.data.Family
 import com.ssafy.zip.android.data.FamilyMember
 import com.ssafy.zip.android.data.Missions
 import com.ssafy.zip.android.databinding.FragmentHomeBinding
+import com.ssafy.zip.android.repository.UserRepository
 import com.ssafy.zip.android.viewmodel.HomeViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -40,8 +44,14 @@ class HomeFragment : Fragment() {
         binding.viewmodel = viewModel
 
         binding.topText.setOnClickListener {
-            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
-            binding.root.findNavController().navigate(action)
+            CoroutineScope(Dispatchers.Main).launch {
+                val instance = UserRepository.getInstance(Application())
+                var response = instance?.logout()
+                if(response.equals("200")){
+                    val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+                    binding.root.findNavController().navigate(action)
+                }
+            }
         }
         binding.mission1Btn.setOnClickListener{
             val action = HomeFragmentDirections.actionHomeFragmentToRecordFragment()
