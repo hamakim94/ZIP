788045f623ae9477df3646f2fc348e89e414fcc3
이재순 <dlwotns1112@gmail.com>
2022-11-10 22:58:42 +0900

    BE_221110_unityserver: photon 가족 연동 완료

--- unity/Assets/Scripts/Data/DataManager.cs
+++ unity/Assets/Scripts/Data/DataManager.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 
 public class DataManager : MonoBehaviour
 {
+    #region Public Fields
     public static DataManager Instance; // 객체를 만들지 않고도 다른 곳에서 꺼내쓸 수 있음. 
     public Dictionary<long, RawData[]> totalItemDicData;
     public Dictionary<long, RawData[]> userItemDicData;
@@ -14,19 +15,27 @@ public class DataManager : MonoBehaviour
     public Dictionary<long, RawData> albumDicData;
     public Dictionary<long, RawData> userAlbumDicData;
     public Dictionary<long, RawData> dicData;
-    public Texture texture;
+    /*public Texture texture;*/
+    public UserInfo user;
+    #endregion
+
+    #region MonoBehaviour Callbacks
     private void Awake()
     {
-        DataManager.Instance = this;
-       
-        LoadTotalItemData();
-        LoadUserItemData();
-        LoadTotalAlbumData();
+        if (Instance == null)
+        {
+            DataManager.Instance = this;
 
-        this.dicData = new Dictionary<long, RawData>();
-        DontDestroyOnLoad(gameObject);
+            LoadTotalItemData();
+            /*LoadTotalAlbumData();*/
+
+            this.dicData = new Dictionary<long, RawData>();
+            DontDestroyOnLoad(gameObject);
+        }
     }
+    #endregion
 
+    #region Public Methods
     // path에 있는 json 파일을 load해서 dictionary 형태로 return 
     public void LoadData(string path) 
     {
@@ -40,54 +49,29 @@ public class DataManager : MonoBehaviour
         }
     }
 
-    private void LoadTotalItemData()
-    {
-        this.totalItemDicData = new Dictionary<long, RawData[]>(); // 위치id : 가구[](가구 list)
-        var ta = Resources.Load<TextAsset>("Data/total_item_data");
-        var json = ta.text;
-        Debug.Log(ta.text);
-        var arrData = JsonConvert.DeserializeObject<PositionData[]>(json);
-
-        foreach(var data in arrData)
-        {
-            this.totalItemDicData.Add(data.id, data.itemList);
-        }
-    }
-    public void LoadUserItemData()
+    public IEnumerator LoadUserItemData()
     {
         this.userItemDicData = new Dictionary<long, RawData[]>(); // 위치id : 사용자가구[](사용자가구 list)
-        var ta = Resources.Load<TextAsset>("Data/user_item_data2"); // api 통신해서 json 가져오기 
-        var json = ta.text;
-        var arrData = JsonConvert.DeserializeObject<PositionUserItemData[]>(json);
-
-        foreach (var data in arrData)
+        var json = "";
+        UnityWebRequest www = APIManager.GetWWW("GET", "/unity/create", null);
+        yield return www.SendWebRequest();
+        if (www.result != UnityWebRequest.Result.Success)
         {
-            this.userItemDicData.Add(data.id, data.itemList);
+            Debug.LogError(www.error);
         }
-    }
-
-    public void LoadTotalAlbumData()
-    {
-        this.albumDicData = new Dictionary<long, RawData>(); // album id : AlbumData
-        var ta = Resources.Load<TextAsset>("Data/total_album_data2"); // api 통신해서 json 가져오기 
-        var json = ta.text;
-        var arrData = JsonConvert.DeserializeObject<AlbumData[]>(json);
-
-        for(int i=0; i<arrData.Length; i++) // album 수
+        else
         {
-            for(int j=0; j<arrData[i].pictures.Length; j++) // 해당 앨범의 사진 수 
+            json = www.downloadHandler.text;
+            var arrData = JsonConvert.DeserializeObject<PositionUserItemData[]>(json);
+
+            foreach (var data in arrData)
             {
-                StartCoroutine(GetTexture(arrData[i].pictures[j]));
+                this.userItemDicData.Add(data.id, data.itemList);
             }
         }
-
-        foreach(var data in arrData)
-        {
-            this.albumDicData.Add(data.id, data);
-        }
     }
 
-    public void LoadUserAlbumData()
+    /*public void LoadUserAlbumData()
     {
         this.userAlbumDicData = new Dictionary<long, RawData>(); // 앨범 pos id : UserAlbumData
         var ta = Resources.Load<TextAsset>("Data/user_album_data2"); // api 통신해서 json 가져오기 
@@ -99,7 +83,7 @@ public class DataManager : MonoBehaviour
 
             this.userAlbumDicData.Add(data.id, data);
         }
-    }
+    }*/
 
     public RawData itemIdToItem(long positionId, long itemId)
     {
@@ -118,14 +102,31 @@ public class DataManager : MonoBehaviour
 
     public void photoIdToPhoto(long albumId, long photoId) { 
     }
+    #endregion
 
-    IEnumerator GetTexture(PhotoData picture)
+    #region Private Methods
+    private void LoadTotalItemData()
+    {
+        this.totalItemDicData = new Dictionary<long, RawData[]>(); // 위치id : 가구[](가구 list)
+        var ta = Resources.Load<TextAsset>("Data/total_item_data");
+        var json = ta.text;
+        Debug.Log(ta.text);
+        var arrData = JsonConvert.DeserializeObject<PositionData[]>(json);
+
+        foreach(var data in arrData)
+        {
+            this.totalItemDicData.Add(data.id, data.itemList);
+        }
+    }
+    #endregion
+
+    /*IEnumerator GetTexture(PhotoData picture)
     {
         UnityWebRequest www = UnityWebRequestTexture.GetTexture(picture.url);
         yield return www.SendWebRequest();
 
         picture.texture = DownloadHandlerTexture.GetContent(www);
-    }
+    }*/
 
     /*public static IEnumerator GetTexture(string url, AlbumListItem album)
     {
