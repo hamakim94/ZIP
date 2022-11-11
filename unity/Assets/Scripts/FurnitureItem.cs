314201b7ec4f066de374deed330e002c35ea3273
이승연 <seongyeon38@gmail.com>
2022-11-11 14:47:51 +0900

    FE_221111_furnitureitem : 가구 item ui 만드는중

---
+++ unity/Assets/Scripts/FurnitureItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+using TMPro;
+
+public class FurnitureItem : MonoBehaviour
+{
+    public Image imgIcon;
+    public TMP_Text imgName;
+    public SpriteAtlas atlas;
+    public GameObject ownedItemBtn;
+    public GameObject usedItemBtn;
+    public GameObject locked;
+    public TMP_Text priceText;
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
