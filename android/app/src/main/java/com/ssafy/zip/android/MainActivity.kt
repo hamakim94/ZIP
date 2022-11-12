275ce02f9799dc23b5f41c2ea8ad353680780075
김민균 <mingyun0324@gmail.com>
2022-11-12 22:24:43 +0900

    FE_221111_board : 방 생성 안 하고 나갔을 때 오류 수정, UI수정 등

--- android/app/src/main/java/com/ssafy/zip/android/MainActivity.kt
+++ android/app/src/main/java/com/ssafy/zip/android/MainActivity.kt
@@ -13,10 +13,15 @@ import androidx.navigation.ui.setupWithNavController
 import com.ssafy.zip.android.data.User
 import com.ssafy.zip.android.databinding.ActivityMainBinding
 import android.Manifest
+import android.app.Application
 import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
+import com.ssafy.zip.android.repository.HomeRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.*
 
 
@@ -31,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         // 네비게이션 호스트
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
@@ -38,9 +44,28 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         // 자동 로그인
         if (!App.prefs.getString("accesstoken", "").equals("")) {
-            val navGraph = navController.navInflater.inflate(R.navigation.bottom_bar_nav_graph)
-            navGraph.setStartDestination(R.id.homeFragment)
-            navController.setGraph(navGraph, null)
+            CoroutineScope(Dispatchers.Main).launch {
+                val instance = HomeRepository.getInstance(Application())
+                var response = instance?.getUserData()
+                println(response.toString())
+                if (response is User) {
+                    println(response.toString())
+                    if (response.hasFamily) {
+                        val navGraph =
+                            navController.navInflater.inflate(R.navigation.bottom_bar_nav_graph)
+                        navGraph.setStartDestination(R.id.homeFragment)
+                        navController.setGraph(navGraph, null)
+                    }
+
+                }
+            }
+            //여기에 가족코드 있는지 없는지 검사
+
+
+            //조건문
+//            val navGraph = navController.navInflater.inflate(R.navigation.bottom_bar_nav_graph)
+//            navGraph.setStartDestination(R.id.homeFragment)
+//            navController.setGraph(navGraph, null)
         }
         // 바인딩
         binding.bottomNavigationView.setupWithNavController(navController)
@@ -48,11 +73,12 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController, false)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             // 특정화면에서 하단바 없애기
-            if (destination.id == R.id.loginFragment || destination.id == R.id.signupFragment || destination.id == R.id.signUpCompleteFragment || destination.id == R.id.recordBoardCreateFragment || destination.id == R.id.recordLetterCreateFragment || destination.id == R.id.recordQnaDetailFragment || destination.id == R.id.recordBoardDetailFragment) {
+            if (destination.id == R.id.loginFragment || destination.id == R.id.signupFragment || destination.id == R.id.signUpCompleteFragment || destination.id == R.id.recordBoardCreateFragment || destination.id == R.id.recordLetterCreateFragment || destination.id == R.id.recordQnaDetailFragment || destination.id == R.id.recordBoardDetailFragment
+                || destination.id == R.id.familyEnterFragment || destination.id == R.id.familyCreateFragment) {
                 binding.bottomNavigationView.visibility = View.GONE
                 binding.bottomAppBar.visibility = View.GONE
                 binding.fab.visibility = View.GONE
-                binding.mainFlContainer.setPadding(0,0,0,0)
+                binding.mainFlContainer.setPadding(0, 0, 0, 0)
                 // 여기에 작성 페이지, 패딩을 0으로 넣으면 될 듯 ^^;
             } else {
                 binding.bottomNavigationView.visibility = View.VISIBLE
@@ -60,7 +86,7 @@ class MainActivity : AppCompatActivity() {
                 binding.fab.visibility = View.VISIBLE
                 val dm = resources.displayMetrics
                 val size = Math.round(56 * dm.density)
-                binding.mainFlContainer.setPadding(0,0, 0,size)
+                binding.mainFlContainer.setPadding(0, 0, 0, size)
                 // dp로 나타내야할듯
             }
         }
@@ -88,7 +114,7 @@ class MainActivity : AppCompatActivity() {
     ) { isGranted: Boolean ->
         if (isGranted) {
             var token = FirebaseMessaging.getInstance().token;
-            println("bbbbbbbbb권한 획득" )
+            println("bbbbbbbbb권한 획득")
             // FCM SDK (and your app) can post notifications.
         } else {
             // TODO: Inform user that that your app will not show notifications.
