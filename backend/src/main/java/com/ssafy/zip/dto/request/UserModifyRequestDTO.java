140855cbb07227d65e23cd5664340e08a0162e82
류현수 <neillie23@gmail.com>
2022-11-15 13:15:57 +0900

    BE_221115_debug : debugging (완료)

--- backend/src/main/java/com/ssafy/zip/dto/request/UserModifyRequestDTO.java
+++ backend/src/main/java/com/ssafy/zip/dto/request/UserModifyRequestDTO.java
@@ -3,5 +3,5 @@ package com.ssafy.zip.dto.request;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-public record UserModifyRequestDTO(@Valid String nickname, @Valid String familyName, @NotNull Long characterId) {
+public record UserModifyRequestDTO(@Valid String nickname, @Valid String familyName, @NotNull(message = "여기가 문제다?") Long characterId) {
 }
