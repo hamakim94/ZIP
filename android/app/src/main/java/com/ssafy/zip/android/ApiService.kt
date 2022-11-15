f9ba6b6c17ba33a64af6277681ed33a557c4398d
이도엽 <lx460@naver.com>
2022-11-16 08:11:17 +0900

    FE_221116_token : token 테스트 진행중

--- android/app/src/main/java/com/ssafy/zip/android/ApiService.kt
+++ android/app/src/main/java/com/ssafy/zip/android/ApiService.kt
@@ -25,12 +25,16 @@ object ApiService {
 
     class TokenInterceptor : Interceptor {
         override fun intercept(chain: Interceptor.Chain): Response {
+            println("1토큰인터셉터시작"+ chain.request());
             val request = chain.request().newBuilder()
                 .addHeader("ACCESSTOKEN", App.prefs.getString("accesstoken", ""))
                 .addHeader("REFRESHTOKEN", App.prefs.getString("refreshtoken", ""))
                 .addHeader("Connection", "close")
                 .build()
-            return chain.proceed(request)
+            println("1리턴값")
+            var response = chain.proceed((request));
+            println("2리턴값"+ response)
+            return response
         }
     }
 
@@ -38,26 +42,30 @@ object ApiService {
         override fun intercept(chain: Interceptor.Chain): Response {
             var request = chain.request();
             var response = chain.proceed(request);
+            println(request);
             when (response.code()) {
                 403 -> {
+                    println("3403 시작")
                     App.prefs.setString("accesstoken", "")
                     CoroutineScope(Dispatchers.Default).launch {
-                        if (response.isSuccessful) {
+                        println("4코루틴 시작")
                             val responseData = getApiService.tokenReissue()
+                        println("5" + responseData.code())
                             val headers = responseData.headers()
+                        println("6" + headers)
                             val accesstoken = headers.get("ACCESSTOKEN").toString()
                             App.prefs.setString("accesstoken", accesstoken)
-                            val request = chain.request().newBuilder().addHeader("ACCESSTOKEN", accesstoken)
-                            .addHeader("REFRESHTOKEN", App.prefs.getString("refreshtoken", ""))
-                                .addHeader("Connection", "close").build()
-                            response = chain.proceed(request)
-                        }
+//                            val request = chain.request().newBuilder().addHeader("ACCESSTOKEN", accesstoken)
+//                            .addHeader("REFRESHTOKEN", App.prefs.getString("refreshtoken", ""))
+//                                .addHeader("Connection", "close").build()
+//                            response = chain.proceed(request)
+
                     }
                 }
                 410 -> {
                     App.prefs.setString("accesstoken", "")
                     App.prefs.setString("refreshtoken", "")
-                    return response
+//                    return response
                 }
             }
             return response
@@ -90,8 +98,8 @@ object ApiService {
 
     // 인터셉터 설정을 위한 okhttp3
     val client = OkHttpClient.Builder()
-        .addInterceptor(TokenInterceptor())
-        .addInterceptor(RefreshInterceptor())
+        .addInterceptor(ApiService.TokenInterceptor())
+        .addInterceptor(ApiService.RefreshInterceptor())
         .build()
 
     private val getApi by lazy {
