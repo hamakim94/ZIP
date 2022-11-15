001fe88bdb1d0fed96d6202df28dff312d0d8434
류현수 <neillie23@gmail.com>
2022-11-15 13:09:43 +0900

    BE_221115_debug : @Valid->@NotNull (완료)

--- backend/src/main/java/com/ssafy/zip/dto/request/UserModifyRequestDTO.java
+++ backend/src/main/java/com/ssafy/zip/dto/request/UserModifyRequestDTO.java
@@ -1,6 +1,7 @@
 package com.ssafy.zip.dto.request;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
-public record UserModifyRequestDTO(@Valid String nickname, @Valid String familyName, @Valid Long characterId) {
+public record UserModifyRequestDTO(@Valid String nickname, @Valid String familyName, @NotNull Long characterId) {
 }
