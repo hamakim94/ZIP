bb5a0289c7bd4da4cd1f51b54744d8423b72a3d4
류현수 <neillie23@gmail.com>
2022-11-17 17:38:56 +0900

    BE_221117_mission : 미션 완료 문구 변경(완료)

--- backend/src/main/java/com/ssafy/zip/util/NotificationEnum.java
+++ backend/src/main/java/com/ssafy/zip/util/NotificationEnum.java
@@ -13,7 +13,7 @@ public enum NotificationEnum {
     PictureUploaded("%s님이 사진을 올렸습니다. 확인해보세요!","myapp://zip.com/board"),
     FamilyEnrolled("%s님이 가족이 되었습니다. 확인해보세요!","myapp://zip.com/home"),
     ScheduleRegistered("%s님이 일정을 등록했습니다. 확인해보세요!","myapp://zip.com/calendar"),
-    QnaMissionAccomplished("가족과 함께하는 ‘오늘의 백문백답’ 미션을 성공했습니다!","myapp://zip.com/home"),
+    QnaMissionAccomplished("가족과 함께하는 ‘오늘의 백문백답’ 미션 성공!\n(300포인트 획득!)","myapp://zip.com/home"),
     TodayLetterMissionAccomplished("가족과 함께하는 ‘오늘의 편지’ 미션을 성공했습니다!","myapp://zip.com/home"),
     ScheduleRegistedForMe("%s님이 함께하는 일정을 등록했습니다. 확인해보세요!","myapp://zip.com/calendar"),
     TodayLetterSentTome("%s님에게 오늘의 편지가 도착했습니다. 확인해보세요!","myapp://zip.com/letter/%s"),
